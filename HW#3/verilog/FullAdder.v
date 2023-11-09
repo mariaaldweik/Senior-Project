d4c5d06002c648f3000e1e7d5bb58bb5e36cb148
abdulazizabusaada <135661560+abdulazizabusaada@users.noreply.github.com>
2023-11-09 11:48:18 +0300

    Update FullAdder.v

--- HW#3/verilog/FullAdder.v
+++ HW#3/verilog/FullAdder.v
@@ -1,8 +1,4 @@
-/**
- * Computes the sum of three bits.
- */
 
-`default_nettype none
 module FullAdder(
 	input a,		//1-bit input
 	input b,		//1-bit input
@@ -10,7 +6,15 @@ module FullAdder(
 	output sum,		//Right bit of a + b + c
 	output carry	//Left bit of a + b + c
 );
+wire w1,w2,w3;
 
-	// Put your code here:
+ // Computes the sum of three bits.
+     xor x1(w1,a,b);
+     xor x2(sum,w1,c);
+     and a1(w2,w1,c);
+     and a2(w3,a,b);
+     or o1(carry,w2,w3);
+
 
 endmodule
+
