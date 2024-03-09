f0f53dbc6091f62d4fa62ac98d189654d260b81f
MARYA E ALDWEIK <140917783+mariaaldweik@users.noreply.github.com>
2024-03-09 14:34:43 +0300

    Update Memory.v

--- HW#7/Memory.v
+++ HW#7/Memory.v
@@ -1,15 +1,12 @@
-
 module Memory(
     input writeM,
+	input clk,
     input [15:0] outM,
     input [15:0] addressM,
-    input clk,
-    output [15:0] inM,
-
-output [15:0] result 
+    output [15:0] inM
 );
 wire loadRAM,loadIO0,loadIO1,loadIO2,loadIO3,loadIO4,loadIO5,loadIO6,loadIO7,loadIO8,loadIO9,loadIOA,loadIOB,loadIOC,loadIOD,loadIOE,loadIOF;
-wire [15:0] inRAM,inIO0,inIO1,inIO2,inIO3,inIO4,inIO5,inIO6,inIO7,inIO8,inIO9,inIOA,inIOB,inIOC,inIOD,inIOE,inIOF,sevenseg;
+wire [15:0] inRAM,inIO0,inIO1,inIO2,inIO3,inIO4,inIO5,inIO6,inIO7,inIO8,inIO9,inIOA,inIOB,inIOC,inIOD,inIOE,inIOF;
 MemoryDecoder memdec(.address(addressM),
 	.load(writeM),
 	.out(inM),
@@ -47,7 +44,6 @@ MemoryDecoder memdec(.address(addressM),
 	.inIOD(inIOD),
 	.inIOE(inIOE),
 	.inIOF(inIOF));
-hex16seg c(outM,sevenseg);
 RAM3840 ram3840(.clk(clk),.address(addressM[11:0]),.in(outM),.load(loadRAM),.out(inRAM));
 Register LED(.clk(clk),.in(outM),.load(loadIO0),.out(inIO0));
 Register BUTTON(.clk(clk),.in(outM),.load(loadIO1),.out(inIO1));
@@ -60,10 +56,11 @@ Register GO(.clk(clk),.in(outM),.load(loadIO7),.out(inIO7));
 Register LCD8(.clk(clk),.in(outM),.load(loadIO8),.out(inIO8));
 Register LCD16(.clk(clk),.in(outM),.load(loadIO9),.out(inIO9));
 Register RTP(.clk(clk),.in(outM),.load(loadIOA),.out(inIOA));
-Register DEBUG0(.clk(clk),.in(sevenseg),.load(loadIOB),.out(result));
-Register DEBUG1(.clk(clk),.in(sevenseg),.load(loadIOC),.out(inIOC));
-Register DEBUG2(.clk(clk),.in(sevenseg),.load(loadIOD),.out(inIOD));
-Register DEBUG3(.clk(clk),.in(sevenseg),.load(loadIOE),.out(inIOE));
-Register DEBUG4(.clk(clk),.in(sevenseg),.load(loadIOF),.out(inIOF));
+Register DEBUG0(.clk(clk),.in(outM),.load(loadIOB),.out(inIOB));
+Register DEBUG1(.clk(clk),.in(outM),.load(loadIOC),.out(inIOC));
+Register DEBUG2(.clk(clk),.in(outM),.load(loadIOD),.out(inIOD));
+Register DEBUG3(.clk(clk),.in(outM),.load(loadIOE),.out(inIOE));
+Register DEBUG4(.clk(clk),.in(outM),.load(loadIOF),.out(inIOF));
+
 
 endmodule
