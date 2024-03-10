e6eca23aaea2aeadd72c7d54aabe7407996e6b54
MARYA E ALDWEIK <140917783+mariaaldweik@users.noreply.github.com>
2024-03-10 13:21:20 +0300

    Update Clock25_Reset20.v

--- HW#7/Clock25_Reset20.v
+++ HW#7/Clock25_Reset20.v
@@ -7,21 +7,19 @@
 module Clock25_Reset20( 
     input CLK,			// external clock 100 MHz	
 	output clk,			// internal clock 25 Mhz
-	output reg reset 		// reset signal approx. 20us
+	output  reset 		// reset signal approx. 20us
 );
 
-wire q1,q2,w0,w1;
-wire [15:0] outreg,w4;
-assign w4=16'b0;
+wire q1,q2,w0,wreset;
+wire [15:0] w1;
+wire [15:0] outreg=16'h000C;
 DFF dff0(.clk(CLK),.in(~q1),.out(q1));
 DFF dff1(.clk(q1),.in(~q2),.out(q2));
 assign clk=q2;
 
    Inc16 in0(.in(outreg),.out(w1));
- Register r0(.clk(CLK),.in(w1),.load(reset),.out(outreg));
-  not (reset,outreg[11]);
- 
+ Register r0(.clk(clk),.in(w1),.load(wreset),.out(outreg));
+
+  not (reset,outreg[9]);
+ assign wreset=reset;
 endmodule
-
-
-
