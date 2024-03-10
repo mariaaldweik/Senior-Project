db7a330d2375ea7b21f7a4665b5ffde1b58915b2
abdulazizabusaada <135661560+abdulazizabusaada@users.noreply.github.com>
2024-03-10 21:46:32 +0300

    Update Clock25_Reset20.v

--- HW#6/Verilog/Clock25_Reset20.v
+++ HW#6/Verilog/Clock25_Reset20.v
@@ -7,25 +7,34 @@
 module Clock25_Reset20( 
     input CLK,			// external clock 100 MHz	
 	output clk,			// internal clock 25 Mhz
-	output reg reset 		// reset signal approx. 20us
+	output  reset 		// reset signal approx. 20us
 );
-reg [11:0] counter=0;
-wire q1,q2;
+
+wire q1,q2,wreset,w7,w00;
+wire [15:0] w1,w4,outreg,w5,w2;
+wire [15:0] w0;
+wire [15:0] w10;
 DFF dff0(.clk(CLK),.in(~q1),.out(q1));
 DFF dff1(.clk(q1),.in(~q2),.out(q2));
 assign clk=q2;
-always @(posedge CLK) 
-begin
-	if(counter>='d2000)
-  reset<=1'b0;
-	else
-	begin
-		counter<=counter+1;
-  reset<=1'b1;
-	end
-	
-end
+assign w00=1'b0;
 
-	// Put your code here:
+assign w10=16'b0000100000000000;
+assign w0=16'b0;
+assign w7=1'b1;
 
+ Inc16 in0(.in(w0),.out(w1));
+
+ Inc16 in1(.in(outreg),.out(w2));
+
+  	
+  Mux16 m2(.a(w1),.b(w2),.sel(wreset),.out(w4));
+
+  Mux16 m3(.a(w4),.b(w10),.sel(outreg[11]),.out(w5));
+
+ Register r0(.clk(CLK),.in(w5),.load(w7),.out(outreg));
+
+  not n1(wreset,outreg[11]);
+assign reset = wreset;
+  
 endmodule
